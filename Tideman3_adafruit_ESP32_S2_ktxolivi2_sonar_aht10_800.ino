03d54158266b12ba4e775a8d520837b91f51f9c4
dalepa <dan.pancamo@gmail.com>
2024-01-23 00:56:27 -0600

    tideman 5.4

---
+++ Tideman3_adafruit_ESP32_S2_ktxolivi2_sonar_aht10_800.ino
@@ -0,0 +1,564 @@
+/*
+    Tdeman 3.0
+    Maker:  Dan Pancamo
+    Updates
+*/
+
+
+String Version = "Tideman OTA V5.4 - AF-ESP32-S2";                       // Version 
+String BoardId = "Tideman3.ktxolivi2-800";         //ESP32 - Victron MPPT 100/30 
+const uint64_t sleepTime = 120e6; // 5 minutes in microseconds
+
+uint64_t lastLogTime=millis();
+
+//DS18B20 Sensor
+// #include <OneWire.h>
+// #include <DallasTemperature.h>
+
+// OneWire oneWire(A0); // DS18B20 on pin A0
+// DallasTemperature sensors(&oneWire);
+
+
+//AHT10
+#include <Adafruit_AHTX0.h>
+#include <Wire.h>
+Adafruit_AHTX0 aht;
+
+
+// ESP DEEP SLEEP
+#include <esp_sleep.h>
+
+
+// MAX17048 battery support
+#include "Adafruit_MAX1704X.h"
+Adafruit_MAX17048 maxlipo;
+
+#include "HardwareSerial.h"
+#include <Adafruit_Sensor.h>
+#include "DHTesp.h"
+
+#include <WiFi.h>
+#include <WiFiUDP.h>
+#include <map>
+#include <algorithm>
+
+#include <string.h>
+
+
+
+// Sonar
+float SENSORHEIGHTINFEET=61/12;  // The hight of the botton of the sensor in feet
+
+bool newData = false; // Whether new data is available from the sensor
+uint8_t buffer[4];  // our buffer for storing data
+uint8_t idx = 0;  // our idx into the storage buffer
+float avg;
+float distance;  // The last measured distance
+float distlow;  // The lowest measured distance
+float disthigh;  // The highest measured distance
+float distmean;  // The middle measured distance
+int count = 0;
+int loopcnt = 0;
+float mDistance[10000];
+
+
+float WaterLevelHigh;
+float WaterLevelLow;
+float WaterLevelAvg;
+
+
+
+String line;
+//WIFI
+
+const char* ssid     = "cam24";
+const char* password = "olivia15";
+
+//UDP
+int port = 8089;
+const char *influxDNS = "bi.pancamo.com";
+IPAddress influxIP;
+WiFiUDP udp;
+
+
+
+int sampleCnt = 0;
+
+//SERIAL ON RX2 on ESP
+// HardwareSerial serialVE(2); // VE.Direct port is connected to UART1
+
+
+//OTA
+#include <ElegantOTA.h> 
+#include <WiFiClient.h>
+#include <WebServer.h>
+WebServer webserver(80);
+
+//WIFIMAN
+uint8_t DisconnectReason=0;
+unsigned long wifiUptime = millis();
+unsigned long wifiDowntime = millis();
+
+
+//DSP11
+DHTesp dht;
+#define DHT11_PIN 27
+
+
+
+String dot2dash(String string) {
+  String new_string = "";
+  for (int i = 0; i < string.length(); i++) {
+    if (string.charAt(i) == '.') {
+      new_string += '-';
+    } else {
+      new_string += string.charAt(i);
+    }
+  }
+  return new_string;
+}
+
+//WIFIMAN
+
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Connected to AP successfully!");
+}
+
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("WiFi connected OTA");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Version: " + Version);
+
+  if (WiFi.hostByName(influxDNS, influxIP)) {
+      Serial.print("Influx IP: ");
+      Serial.println(influxIP);
+    } else {
+      Serial.println("DNS lookup failed for " + String(influxDNS));
+    }
+  
+  wifiDowntime=millis();
+  
+  // reason 0 on 1st status is ok
+  line = String(BoardId + ".wifi.disreason value=" + String(DisconnectReason));
+  toInflux(line);  
+    
+  
+  //line = String(BoardId + ".wifi.localip." + dot2dash(String(WiFi.localIP()))  + " value=" + String(DisconnectReason));
+  //toInflux(line);
+
+}
+
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("Disconnected from WiFi access point");
+  Serial.print("WiFi lost connection. Reason: ");
+  DisconnectReason = info.wifi_sta_disconnected.reason;
+
+  wifiDowntime=millis();
+
+  Serial.println(info.wifi_sta_disconnected.reason);
+  Serial.println("Trying to Reconnect");
+  WiFi.begin(ssid, password);
+}
+
+
+void MAX17048setup()
+{
+  Serial.println(F("\nAdafruit MAX17048 simple demo"));
+
+  if (!maxlipo.begin()) {
+    Serial.println(F("Couldnt find Adafruit MAX17048?\nMake sure a battery is plugged in!"));
+    // while (1) delay(10);
+  }
+  Serial.print(F("Found MAX17048"));
+  Serial.print(F(" with Chip ID: 0x")); 
+  Serial.println(maxlipo.getChipID(), HEX);
+}
+
+
+void wifiSetup()
+{
+
+  // WIFI RECONNECT
+  WiFi.disconnect(true);
+  wifiUptime=millis();
+  wifiDowntime=millis();
+  
+  delay(2000);
+
+  WiFi.onEvent(WiFiStationConnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_CONNECTED);
+  WiFi.onEvent(WiFiGotIP, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_GOT_IP);
+  WiFi.onEvent(WiFiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+
+
+  WiFi.begin(ssid, password);
+    
+  Serial.println();
+  Serial.println();
+  Serial.println("Wait for WiFi... ");
+  
+}
+
+void logWifiStatus()
+{
+  line = String(BoardId + ".wifi.rssi value=" + String(WiFi.RSSI()));
+  toInflux(line);
+
+  wifiUptime=millis()-wifiDowntime; 
+  line = String(BoardId + ".wifi.uptime value=" + String(wifiUptime/1000));
+  toInflux(line);
+
+}
+
+//WIFI REONNECT END-------------------------
+
+//OTA 
+void handle_OnConnect() 
+{
+  webserver.send(200, "text/plain", "Hello from " + Version + " " + BoardId);
+}
+
+void otaSetup()
+{
+  webserver.on("/", handle_OnConnect);
+  ElegantOTA.begin(&webserver);    // Start ElegantOTA
+  webserver.begin();
+}
+
+
+
+
+//Normalize Data, remove the bad data with 5 samples
+std::map<String, int> data[5];
+// Function prototype
+int medianData(std::map<String, int> (&data)[5], String code);
+
+int  medianData(std::map<String, int> (&data)[5], String code) {
+  // Access the values of the map array
+  int numbers[5];
+
+  for (int i = 0; i < 5; i++) {
+    numbers[i] = data[i][code];
+  }
+  std::sort(numbers, numbers + 5);
+  return (numbers[2]);
+}
+
+
+
+int sort_desc(const void *cmp1, const void *cmp2)
+{
+  // Need to cast the void * to int *
+  int a = *((int *)cmp1);
+  int b = *((int *)cmp2);
+  // The comparison
+  return a > b ? -1 : (a < b ? 1 : 0);
+  // A simpler, probably faster way:
+  //return b - a;
+}
+
+float average (float * array, int len)  // assuming array is int.
+{
+  long sum = 0L ;  // sum will be larger than an item, long for safety.
+  for (int i = 0 ; i < len ; i++)
+    sum += array [i] ;
+  return  ((float) sum) / len ;  // average will be fractional, so float may be appropriate.
+}
+
+
+
+
+void toInflux (String line)
+{
+
+      Serial.println(line);
+
+      udp.beginPacket(influxIP, port);
+      udp.print(line);
+      udp.endPacket();
+
+}
+
+void tempToInflux()
+{
+
+      //DHT11
+      float temperature = dht.getTemperature()  * 1.8 + 32 ;
+      float humidity = dht.getHumidity();
+      //Serial.printf("Temp = %2.2f\n", temperature);   
+
+      line = String(BoardId + ".battery.temperature value=" + String(temperature));
+      toInflux(line);
+      line = String(BoardId + ".battery.humidity value=" + String(humidity));
+      toInflux(line);
+
+}
+
+
+void distToInflux(float distance)
+{
+
+      String line = String("tideman3." + BoardId + ".distance value=" + String(distance));
+      toInflux(line);
+
+}
+
+
+void getDistance()
+{
+
+  if (Serial1.available()) {
+
+    uint8_t c = Serial1.read();
+    // Serial1.println(c, HEX);
+
+    // See if this is a header byte
+    if (idx == 0 && c == 0xFF) {
+      buffer[idx++] = c;
+    }
+    // Two middle bytes can be anything
+    else if ((idx == 1) || (idx == 2)) {
+      buffer[idx++] = c;
+    }
+    else if (idx == 3) {
+      uint8_t sum = 0;
+      sum = buffer[0] + buffer[1] + buffer[2];
+      if (sum == c) {
+        distance = ((uint16_t)buffer[1] << 8) | buffer[2];
+        newData = true;
+      }
+      idx = 0;
+    }
+  }
+  
+  if (newData) {
+
+    //Serial.printf("getSonarDistance NEW DATA\n");
+
+    mDistance[count] = distance;
+    count++;
+    
+    //Serial.print("Distance: ");
+    //Serial.print(distance/25.4/12);
+    //Serial.println(" ft");
+    
+    if (count == 1000)
+    {
+      //distToInflux(distance/25.4/12);
+
+      qsort(mDistance, count, sizeof(mDistance[0]), sort_desc);
+
+      disthigh = mDistance[50]/25.4;
+      distlow = mDistance[count-50]/25.4;
+
+      avg = average(mDistance,count)/25.4;
+      count = 0;
+      
+      WaterLevelHigh=SENSORHEIGHTINFEET-((distlow)/12.0);
+      WaterLevelLow=SENSORHEIGHTINFEET-((disthigh)/12.0);
+      WaterLevelAvg=SENSORHEIGHTINFEET-((avg)/12.0);
+
+      // logall();
+
+
+    }
+
+  }
+}
+
+
+void logDistance()
+{
+
+      toInflux(BoardId + ".WaterLevelAvg value=" + String(WaterLevelAvg));
+      toInflux(BoardId + ".WaterLevelHigh value=" + String(WaterLevelHigh));
+      toInflux(BoardId + ".WaterLevelLow value=" + String(WaterLevelLow));
+      toInflux(BoardId + ".DistanceAvg value=" + String(avg/12.0));
+
+
+}
+
+
+void logBattery()
+{      
+        //Serial.print(F("Batt Voltage: ")); Serial.print(maxlipo.cellVoltage(), 3); Serial.println(" V");
+        //Serial.print(F("Batt Percent: ")); Serial.print(maxlipo.cellPercent(), 1); Serial.println(" %");
+        //Serial.println();
+
+      //if(maxlipo.cellPercent() < 110 )
+      {
+        toInflux(BoardId + ".BattVoltage value=" + String(maxlipo.cellVoltage()));
+        toInflux(BoardId + ".BattPercent value=" + String(maxlipo.cellPercent()));
+        toInflux(BoardId + ".BattChargeRate value=" + String(maxlipo.chargeRate()));
+      }
+
+}
+
+
+
+void logTds()
+{
+  
+  int sensorValue = analogRead(A0); // Read analog value from sensor
+  float tdsValue = map(sensorValue, 0, 1023, 0, 1000); // Map to TDS value range
+
+
+  //Serial.printf("sensorValue Value = %d\n",  sensorValue);   
+  //Serial.printf("tdsValue Value = %f\n",  tdsValue); 
+  
+  line = String(BoardId + ".TDS value=" + String(tdsValue));
+  toInflux(line);
+  line = String(BoardId + ".TDSraw value=" + String(sensorValue));
+  toInflux(line);
+
+
+}
+
+void logAHT10()
+{
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);
+  
+  
+
+  float temperature = (float) temp.temperature * 1.8 + 32.0;
+ 
+  line = String(BoardId + ".AHT10.temperature value=" + String(temperature));
+  toInflux(line);
+
+  line = String(BoardId + ".AHT10.humidity value=" + String(humidity.relative_humidity));
+  toInflux(line);
+
+
+/*
+  // Print the temperature and humidity to the serial port
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println("°F");
+  Serial.print("Humidity: ");
+  Serial.print(humidity.relative_humidity);
+  Serial.println("%");
+*/
+
+}
+void AHT10setup()
+{
+      //AHT10 Setup
+    Wire.begin();
+
+    // Initialize the AHT10 sensor
+    if (!aht.begin()) 
+    {
+      Serial.println("Could not find AHT10 sensor!");
+    }
+
+}
+
+
+
+void setup() 
+{
+
+    Serial.begin(9600);
+    Serial1.begin(9600); // Set the baud rate for the VE.Direct port
+
+
+    //WIFI SETUP  
+    wifiSetup();
+
+    //OTA Setup
+    otaSetup();
+
+    // Batter Monitor Setup
+    MAX17048setup();
+
+    // AHT10 Setup
+    AHT10setup();
+
+    lastLogTime = millis();
+
+    //DEEPSLEEP
+    deepSleeplog(60,120);  //LOG TIME, SLEEP TIME
+}
+
+
+
+
+
+
+
+void logall(int loginterval)    //loginterval in seconds
+{
+
+      // log other data  here
+      
+      int elapsed = (millis() - lastLogTime) / 1000;
+
+      if (elapsed > loginterval)
+      {
+        logBattery();
+        logWifiStatus();
+        logAHT10();
+        logDistance();
+
+        lastLogTime = millis();
+
+        Serial.println("logging for seconds: " + String(lastLogTime/1000));
+
+      }
+
+
+}
+
+void deepSleeplog(int seconds, int sleeptime)
+{
+
+  sleeptime = sleeptime*1000000;
+  
+
+  if ( maxlipo.cellPercent() < 50)  //start sleeping at 50%
+  {
+    while (millis() < seconds * 1000)  //log for at least seconds afer deep sleeping
+    {
+      
+      getDistance(); // get distance ASAP
+      logall(5);
+
+      // webserver.handleClient();  //dont allow webserver clients at 50%
+    }
+  
+
+    if (maxlipo.cellPercent() < 10 )
+    {  
+      sleeptime = sleeptime * 5;
+    }
+
+    if (maxlipo.cellPercent() < 30 )
+    {  
+      sleeptime = sleeptime * 2;
+    }
+
+    
+    esp_sleep_enable_timer_wakeup(sleeptime);  //sleeptime in micro x 1,000,000 seconds
+
+    esp_deep_sleep_start();
+    
+  }
+
+  getDistance();
+  logall(5);  //log for seconds .  wont get called if deep sleeping
+
+}
+
+void loop() {
+
+    //Serial.println("inloop!");
+
+    webserver.handleClient();
+    deepSleeplog(60,120); //LOG TIME, SLEEP TIME
+
+
+}
+
+
+
